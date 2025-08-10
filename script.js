640e78740c5e5bc6247fa7a84fe3810837982aa4
Alufena <andersonpolv@gmail.com>
2025-08-10 01:56:10 -0300

    Added tabbed component

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
@@ -13,7 +12,6 @@ const openModal = function (e) {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
-
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
@@ -33,21 +31,60 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
-btnScrollTo.addEventListener('click', function (e) {
+/* btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
   // window.scrollTo( // 1ª maneira de pular até certa parte da página
   //   s1coords.left + window.pageXOffset,
   //   s1coords.top + window.pageYOffset
   // );
-  // window.scrollTo({ // 2ª maneira de pular até certa parte da página
+  // window.scrollTo({ // 2ª maneira
   //   left: s1coords.left + window.pageXOffset,
   //   top: s1coords.top + window.pageYOffset,
   //   behavior: 'smooth',
   // });
-  section1.scrollIntoView({ // 3ª maneira de pular até certa parte da página
+  section1.scrollIntoView({
+    // 3ª maneira
     behavior: 'smooth',
   });
+}); */
+
+/* document.querySelectorAll('.nav__link').forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+    // console.log('test');
+    const id = this.getAttribute('href');
+    // console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  });
+}); */
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  // usando event delegation
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+// tabs.forEach((tab) => tab.addEventListener('click', () => console.log('test'))); // má prática
+
+tabsContainer.addEventListener('click', function (e) {
+  // const btnClicked = e.target;
+  // const btnClicked = e.target.parentElement;
+  const btnClicked = e.target.closest('.operations__tab');
+  // console.log(btnClicked);
+  if (!btnClicked) return; // "guard clause" que serve para evitar erros ao clicar em nada (não nos botões)
+  tabs.forEach((tab) => tab.classList.remove('operations__tab--active'));
+  tabsContent.forEach((content) =>
+    content.classList.remove('operations__content--active')
+  );
+  btnClicked.classList.add('operations__tab--active');
+  document
+    .querySelector(`.operations__content--${btnClicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
